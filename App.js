b66d1794298bade854a13321239a91a40e059feb
Justin Stevens <justin@interactiveshift.com>
2018-07-11 16:35:16 -0700

    init

--- App.js
+++ App.js
@@ -1,13 +1,110 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import styled from 'styled-components';
+
+const RowView = styled.View`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  marginLeft: 5;
+  marginRight: 5;
+`;
+
+const ColumnView = styled.View`
+  flex: 1;
+  marginLeft: 5;
+  marginRight: 5;
+`;
+
+const Input = styled.TextInput`
+  borderColor: #ced4da;
+  borderWidth: 1;
+  paddingTop: 5;
+  paddingBottom: 5;
+  paddingLeft: 5;
+  paddingRight: 5;
+  borderRadius: 2;
+  backgroundColor: white;
+`;
+
+const Label = styled.Text`
+  fontWeight: bold;
+  marginBottom: 5;
+`;
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gratuity: '',
+      damage: '0',
+      tip: '0'
+    };
+  }
+
+  handleDamage(val) {
+    this.setState({
+      damage: val.replace(/[^0-9]/g, ''),
+      tip: this.calc(val, this.state.gratuity)
+    });
+  }
+
+  handleGratuity(val) {
+    this.setState({
+      gratuity: val.replace(/[^0-9]/g, ''),
+      tip: this.calc(this.state.damage, val)
+    });
+  }
+
+  calc(num, per) {
+    return (num / 100)*per;
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <RowView style={styles.mB5}>
+          <ColumnView>
+            <Label style={styles.mB5}>Damage</Label>
+            <Input
+              style={styles.mB5}
+              type="numeric"
+              keyboardType='numeric'
+              onChangeText={(val) => {this.handleDamage(val)}}
+              value={this.state.damage}
+            />
+          </ColumnView>
+        </RowView>
+
+        <RowView style={styles.mB5}>
+          <ColumnView>
+            <Label>Gratuity</Label>
+            <Input
+              onChangeText={(gratuity) => this.handleGratuity(gratuity)}
+              value={this.state.gratuity}
+            />
+          </ColumnView>
+        </RowView>
+
+        <RowView style={styles.mB5}>
+          <ColumnView>
+            <Label>Split</Label>
+            <Input
+              onChangeText={(text) => this.setState({text})}
+              value={this.state.text}
+            />
+          </ColumnView>
+        </RowView>
+
+
+        <RowView style={styles.mB5}>
+          <ColumnView>
+            <Text>Total</Text>
+          </ColumnView>
+          <ColumnView>
+            <Text>{this.state.tip}</Text>
+          </ColumnView>
+        </RowView>
       </View>
     );
   }
@@ -20,4 +117,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  bgDark: {
+    backgroundColor: '#ccc'
+  },
+  mB5: {
+    marginBottom: 5
+  }
 });
